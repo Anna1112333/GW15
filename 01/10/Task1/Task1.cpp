a2da7a3a64a68d9537f506b38d1df620ddefc0f0
Anna1112333 <anna_1112333@mail.ru>
2022-11-12 14:22:15 +0500

    Попытка с for(***:***)

--- 01/10/Task1/Task1.cpp
+++ 01/10/Task1/Task1.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main()
 {//С нечётными номерами умножаются на 3
     int arr[] = { 2,3,7,9,11 };
-    for(int n: arr)
-    cout << n<<" ";
+    int n, k;
+    for (int f : arr)
+    {
+        f = 3*f; 
+        cout << *(&f+sizeof(int)) << " ";
+    }
 }
 

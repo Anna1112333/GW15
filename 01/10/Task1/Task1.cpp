c45d7500e1dcb7b73b4bc271b0e41c25fee0eebc
Anna1112333 <anna_1112333@mail.ru>
2022-11-18 20:38:51 +0500

    1

--- 01/10/Task1/Task1.cpp
+++ 01/10/Task1/Task1.cpp
@@ -1,14 +1,24 @@
 ﻿#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 using namespace std;
 
 int main()
 {//С нечётными номерами умножаются на 3
-    int arr[] = { 2,3,7,9,11 };
-    int n, k;
-    for (int f : arr)
+    vector <int> arr;
+    arr= { 2,3,7,9,11 };
+    vector <int>::iterator n;
+    //int n=0, k;
+    n = arr.begin();
+    transform(arr.begin(), arr.end(), arr.begin(),
+        [n](vector <int> arr)mutable { advance(n, 3); return arr; });
+    for (long int f : arr)
     {
-        f = 3*f; 
-        cout << *(&f+sizeof(int)) << " ";
+        n++;
+       // if ((n + 1) % 2) f *= 3;
+        cout << f << " ";
+        //cout << *(&f+sizeof(int)) << " ";
     }
 }
 

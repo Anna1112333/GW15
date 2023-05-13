d97be5137ad1e5de6415897d09a4b620b949499f
Anna1112333 <anna_1112333@mail.ru>
2023-05-13 20:51:19 +0500

    Ориентированне графы

---
+++ Alg0108/Alg0108/Alg0108.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int n;
+
+    ifstream gh("Matr.txt");
+    if (gh.is_open())
+    {
+        gh >> n; // читаем из файла
+    }
+    else
+    {
+        std::cout << "Не получилось открыть файл!" << std::endl;
+    }
+    int** a = new int* [n];
+    cout << n << endl;
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            gh >> a[i][j];           
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int k=0;
+        cout << i+1 << "  ";
+        for (int j = 0; j < n; j++)
+            if (a[i][j]) {
+                k++;
+                cout << j + 1 << " ";
+            }
+        if (k == 0) cout << "нет";
+        cout<<"\n"; 
+    }
+    gh.close();
+    for (int i = 0; i < n; i++)
+    {
+        delete[] a[i];
+    }
+    delete[] a;
+    return 0;
+}
---
+++ Alg0208/Alg0208/Alg0208.cpp
@@ -0,0 +1,54 @@
+﻿#include <fstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> end_top(int v, int** arr, int n)
+{
+    vector<int> temp;
+    temp.push_back(v);
+    int i = 0, m=-1;
+    for (; i < n; ++i)
+        if (arr[v][i] == 1) { v = i; i = 0;  temp.push_back(v);
+    }
+      //  else { m = v; i = n; }
+    return temp;
+}
+
+
+
+int main()
+{
+    int n, first;
+    setlocale(LC_ALL, "Rus");
+    ifstream GR("Graph.txt");
+    if (GR.is_open()) { GR >> n; }
+    else throw "File is not open!";
+     cout << "Введите номер вершины с которй нухно начать от 1 до "<<n<<": \n";
+     cin>>first;
+     int* used_top = new int[n] {};
+     int** arr = new int*[n];
+     for(int i=0; i<n;++i)
+     {
+         arr[i] = new int[n];
+         cout << endl;
+         for (int j = 0; j < n; ++j)
+         {
+             GR >> arr[i][j];
+             cout << arr[i][j] << " ";
+         }
+     }
+     //вывод последовательных вершин до тупиковой
+     vector<int> trase = end_top(first, arr, n);
+     for (int x : trase) cout << x;
+
+
+
+
+     GR.close();
+     delete[] used_top;
+     for (int i = 0; i < n; ++i)
+         delete[] arr[i];
+     delete[] arr;
+}
+

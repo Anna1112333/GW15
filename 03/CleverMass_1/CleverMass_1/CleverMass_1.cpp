8d44e371dd5c07c5f8338038516757b0ee7685d5
Anna1112333 <anna_1112333@mail.ru>
2023-05-31 23:35:57 +0500

    Второе задание путаница в указателе - дорабатываю

---
+++ 03/CleverMass_1/CleverMass_1/CleverMass_1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+class smart_array{
+	int arr_size, ite_arr=0;
+	int* arr0;
+	public:
+		smart_array(int arr_size)
+		{
+			arr0 = new int[arr_size];
+			this->arr_size = arr_size;
+		}
+		~smart_array() { delete [] arr0; }
+		void add_element(int elem_arr) {
+			if (ite_arr < arr_size)
+			{
+				arr0[ite_arr] = elem_arr;
+				ite_arr++;
+			}
+			else std::cout << "Элемент вышел за рамки массиа";
+		}
+		int get_element(int index) {
+			if (index > 0 && index < ite_arr) return arr0[index];
+			else  throw "ошибка в номере";
+		}
+};
+
+int main()
+{
+   
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(1) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
---
+++ 03/CleverMass_2/CleverMass_2/CleverMass_2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+class smart_array {
+	int arr_size, ite_arr = 0, copasity_objects=0;
+	int** arr0[10];
+	
+public:
+	smart_array(int arr_size)
+	{
+		arr0[copasity_objects] = new int*[arr_size];
+		this->arr_size = arr_size;
+	
+		copasity_objects++;
+	}
+	~smart_array() {
+		for(int i=0; i< copasity_objects; i++)
+		delete[] arr0[i];
+		delete[] arr0; 
+	}
+	void add_element(int elem_arr) {
+		if (ite_arr < arr_size)
+		{
+			arr0[copasity_objects][ite_arr] = elem_arr;
+			ite_arr++;
+		}
+		else std::cout << "Элемент вышел за рамки массиа";
+	}
+	int get_element(int index) {
+		if (index > 0 && index < ite_arr) return arr0[copasity_objects][index];
+		else  throw "ошибка в номере";
+	}
+};
+
+int main()
+{	
+	smart_array arr(5);
+	arr.add_element(1);
+	arr.add_element(4);
+	arr.add_element(155);
+
+	smart_array new_array(2);
+	new_array.add_element(44);
+	new_array.add_element(34);
+
+	arr = new_array;
+
+}

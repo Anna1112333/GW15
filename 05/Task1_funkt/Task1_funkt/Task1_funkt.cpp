dc8c32a98f0fcb1ecc637f253350be481c23872a
Anna1112333 <anna_1112333@mail.ru>
2023-07-07 22:45:37 +0500

    1 задание исправила

--- 05/Task1_funkt/Task1_funkt/Task1_funkt.cpp
+++ 05/Task1_funkt/Task1_funkt/Task1_funkt.cpp
@@ -2,42 +2,43 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
-//using namespace std;
+
 template<typename T>
 
-void sqw(T a)
+void sqw(T& a)
 {
-    a = a * a;
-    std::cout << a<<std::endl;
-
+    a = a * a; 
 }
-template<> void sqw<std::vector<int>> (std::vector<int> arr)
-{
-    std::vector <int>::iterator n;
-
+void sqw(std::vector<int>& arr)
+{  
+    std::vector<int>::iterator n;
     int  k = distance(arr.begin(), arr.end());
-
-    n = arr.begin();
-
-    for (int f : arr)
-    {
-        std::cout << f << " ";
-    }
-    std::cout << std::endl;
+     n = arr.begin();  
     int ct = 0; // Обращаемся к счётчику ct, t - значение элемента вектора
     std::transform(n, (n + k), n, [&ct](int t)mutable {
          t = t * t;
         ct++;
         return t;
         }
-    );
-    for (int f : arr)  std::cout << f << " ";
-    std::cout << std::endl;
+    );  
 }
 int main()
 {
-    int g = 1000;
+    int g = 10;
+    std::cout<<g<< std::endl;
     sqw(g);
+    std::cout << g << std::endl;
+    sqw(g);
+    std::cout << g << std::endl;
+    std::cout << std::endl;
+
     std::vector<int> a{ 1,6,8 };
-    sqw(a);    
+    for (int f : a) { std::cout << f << " "; }
+    std::cout << std::endl;
+    sqw(a); 
+    for (int f : a) { std::cout << f << " "; }
+    std::cout << std::endl;
+    sqw(a);
+    for (int f : a) { std::cout << f << " "; }
+    std::cout << std::endl;
 }

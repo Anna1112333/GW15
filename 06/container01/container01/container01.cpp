c878c1596b74f30a5ec3dfc701200cbe76d1b786
Anna1112333 <anna_1112333@mail.ru>
2023-07-10 00:50:02 +0500

    Задания 1 2

---
+++ 06/container01/container01/container01.cpp
@@ -0,0 +1,37 @@
+﻿#include<utility>
+#include<iomanip>
+#include<map>
+#include<vector>
+#include <iostream>
+#include<algorithm>
+class massive
+{
+    std::pair<char, int> par;
+public:
+    std::map<char, int> simbols;
+    massive(std::string mas) 
+    {
+        for (std::string::iterator i = mas.begin(); i != mas.end(); ++i)
+        {
+            par=std::make_pair (*i, 1);
+            if (simbols.contains(par.first)) simbols[*i]++;
+            else simbols.insert(par);
+        }
+ for(int i=15; i>0;--i)
+        for(std::map<char, int >::iterator it = simbols.begin();
+    it != simbols.end(); ++it)
+{
+     if(i==it->second)
+    std::cout << it->first << " " << it->second << " " <<  "\n";
+}
+    }
+};
+int main()
+{
+    std::string str;
+    setlocale(LC_ALL, "RUS");
+    std::cout << "Введите фразу.\n";
+    std::cin >> str;
+    massive b(str);
+}
+
---
+++ 06/container02/container02/container02.cpp
@@ -0,0 +1,24 @@
+﻿#include<set>
+#include<vector>
+#include <iostream>
+#include<deque>
+
+
+int main()
+{
+    int a, b;
+    std::set<int> c;
+    std::deque <int> d;
+    setlocale(LC_ALL, "RUS");
+    std::cout << "Введите число элементов.\n";
+    std::cin >> a;
+    std::cout << "Вводите числа.\n";
+    for (int i = 0; i < a; i++)
+    {
+        std::cin >> b;
+        c.insert(b);
+    }
+    for (auto j : c) d.push_front(j);
+    std::cout << "Ваши числа:" << std::endl;
+    for (auto f : d) std::cout<<f<<std::endl;
+}

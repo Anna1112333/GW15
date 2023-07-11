229fb2adfa97bc3bd6cadad3dc296f114d15ba26
Anna1112333 <anna_1112333@mail.ru>
2023-07-11 17:06:37 +0500

    STL задания'

---
+++ 07/SLNvec02/SLNvec02/SLNvec02.cpp
@@ -0,0 +1,28 @@
+﻿#include<vector>
+#include<string>
+#include<set>
+#include<list>
+
+#include <iostream>
+
+template<typename T>
+void  print_container(T& a)
+{
+    for (std::string g : a)
+        std::cout << g<<" ";
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two. помните почему такой порядок? :)
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+}
---
+++ 07/STLvec01/STLvec01/STLvec01.cpp
@@ -0,0 +1,30 @@
+﻿#include<algorithm>
+#include<vector>
+#include <iostream>
+void vec(std::vector<int> m)
+{
+    std::sort(m.begin(), m.end());
+    std::vector<int>::iterator i;
+    i = m.begin();
+    int t = *(m.end()-1);
+    std::unique(i, m.end());
+   do {
+        i++;
+    } while (*i != t);
+    m.erase(i+1, m.end());
+    std::cout << "[OUT]: ";
+    for (int q : m) std::cout << q << " ";
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    std::vector<int>a{5, 55, 6, 8, 5, 6, 4, 8, 4, 1, 2, 6, 6, 4, 22};
+    std::cout << "[IN]: ";
+    for (int i : a)
+        std::cout << i << " "; 
+    std::cout<<std::endl;
+    vec(a);
+
+}

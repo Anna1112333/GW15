9847cb714856099020daa55fc6c130439f1d0226
Anna1112333 <anna_1112333@mail.ru>
2023-09-08 21:09:44 +0500

    Пока 1 поток во втором задании

---
+++ Thread_00/Thread_1/Thread_1/Thread_1.cpp
@@ -0,0 +1,38 @@
+﻿#define _USE_MATH_DEFINES
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <Windows.h>
+#include <execution>
+
+
+int main()
+{
+   int n = 15; //n - клиеты увеличивающие  счётчик на 1 в секунду один раз
+   int count = 0; //начальное значение счётчика
+   std::thread t1([&]() {
+       while(n!=0){
+       using namespace std::chrono_literals;
+       //using namespace ConsoleParams; consol_parameter::SetColor(0, 10);
+           count--;
+           std::this_thread::sleep_for(2s);
+           std::cout << " " << count;
+       }
+       });
+   std::thread t2([&]() {
+       using namespace std::chrono_literals;
+       while(n!=0){
+       std::this_thread::sleep_for(1s);      
+           count++;
+           std::cout << " " << count;
+           n--;
+       }
+       });   
+   t1.join();
+   t2.join();
+   using namespace std::chrono_literals;
+  // std::this_thread::sleep_for (20s);
+    setlocale(LC_ALL, "RUS");
+    std::cout << "\nСчёт завершён!\n";    
+}
+
---
+++ Thread_00/Thread_2/Thread_2/Thread_2.cpp
@@ -0,0 +1,82 @@
+﻿#include <thread>
+#include <chrono>
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+ void summa(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b, int n, int m)
+ {
+     for (int k = 0; k < 5; k++) {
+
+         for (int i = 0; i < n; i++)
+         {
+             std::vector<int> va, vb; //временный вектор в области видимости
+             va.resize(m); vb.resize(m); //нужно заранее уквзать длину, чтоб не выходить за границы при заполнении
+             for (int j = 0; j < m; j++)
+             {
+                 va[j] = rand() % 10;
+                 vb[j] = rand() % 10;
+                 //  std::cout <<va[j]<< " + " << vb[j] << "\t";
+             }   // std::cout << "\n";
+             a.push_back(va); b.push_back(vb);
+         }
+
+         std::vector < std::vector<int>>s;
+         auto start = std::chrono::steady_clock::now();
+         for (int i = 0; i < n; i++)
+         {
+             std::vector<int> s1;
+             s1.resize(m);
+             for (int j = 0; j < m; j++)
+             {
+                 s1[j] = b[i][j] + a[i][j];
+             }
+             s.push_back(s1);
+         }
+         /*  for (int i = 0; i < n; i++)
+           {
+               for (int j = 0; j < m; j++)
+               {
+                   std::cout << s[i][j] << "\t";
+               } std::cout << std::endl;}*/
+
+
+         auto end = std::chrono::steady_clock::now();
+         std::chrono::duration<double, std::milli> some = end - start;
+         std::cout << "time is " << some.count() << std::endl;
+         n = n * 10;
+     } std::cout << "\t";
+}
+
+ int main()
+ {
+     std::cout << std::thread::hardware_concurrency() << std::endl;
+     const int n = 10, m = 100;
+    
+     std::vector<std::vector<int>> a, b;
+    std::vector<int> a1, b1;
+   
+   
+   
+     /*   for (int i = 0; i < n; i++)
+        {
+            std::vector<int> va, vb; //временный вектор в области видимости
+            va.resize(m); vb.resize(m); //нужно заранее уквзать длину, чтоб не выходить за границы при заполнении
+            for (int j = 0; j < m; j++)
+            {
+                va[j] = rand() % 10;
+                vb[j] = rand() % 10;
+                //  std::cout <<va[j]<< " + " << vb[j] << "\t";
+            }   // std::cout << "\n";
+            a.push_back(va); b.push_back(vb);
+        }*/
+        std::thread t1(summa, b, a, n, m);
+    
+    //std::thread t2(summa, s[0], b[0], a[0], m, n);
+   // summa(s[0], b[0], a[0], m, n );
+   
+     
+    t1.join();
+    //t2.join();    
+}
+

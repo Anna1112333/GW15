4d5a9cb4c6779027df777e8123f6b20e0238b70c
Anna1112333 <anna_1112333@mail.ru>
2024-05-29 23:27:28 +0500

    задания 1испр 2

--- Thread_00/Thread3_1/Task1/Task1/Task1.cpp
+++ Thread_00/Thread3_1/Task1/Task1/Task1.cpp
@@ -5,11 +5,10 @@
 #include <utility>
  using It=std::vector<int>::iterator ;
  int m;
+ It it0, it1, Im;
 
-class sorti {
-   
-    It it0, it1, Im;
-    It find(It a, It b) {       
+
+ It find(It a, It b) {       
         for (; a < b; a++)
         {
             if (m > *a) {
@@ -21,19 +20,18 @@ class sorti {
         std::cout << "min = " << m << std::endl;
         return Im;
     }
-    public:
+
+   
     void sort(std::vector<int>& a, int n) {
         Im = a.begin()+n;
         m = a[n];
-        
         int s = a.size();
         it0=a.begin()+n;
         if(it0<a.end() - 5)
         it1 = it0 + 4;
         if(s>5)
        while(it1 < a.end()-5)
-        {
-          // std::future<It> ft = std::async(find, ref(it0), ref(it1));
+        {        
            find(it0, it1);         
                 it1 += 5;
                 it0 += 5;
@@ -46,21 +44,17 @@ class sorti {
         for (auto s : a)
             std::cout << s << " ";
      }
+
+
     void sort_all(std::vector<int>& a0) {
         int s = a0.size();
-        for (int i = 0; i<s; i++) {
-            sort(a0,i);      
-        }
+        for (int i = 0; i<s; i++)     
+        std::future<void> ft= async(sort, std::ref(a0), i);        
     } 
-};
-
 
 
 int main()
 {
-    std::vector<int> arr{ 10,7,6,13,8,  4, 19, 20, 5,14 ,   4, 6, 4, 1, 5,    12, 5 };
-    sorti a;
-   a.sort_all(arr);
-   // for (auto x : arr)
-      //  std::cout << *it << " ";
+    std::vector<int> arr{ 10,7,6,13,8,  4, 19, 20, 5,14 ,   4, 6, 4, 1, 5,    12, 5 };  
+    sort_all(arr);   
 }
---
+++ Thread_00/Thread3_2/Task2/Task2/Task2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>   //ищем максимальное нечётное число
+#include <future>
+#include <mutex>
+#include <vector>
+#include <iterator>
+using It = std::vector<int>::iterator ;
+int t = 0;
+It f;
+std::mutex n;
+
+void find(It a, It b) {
+    while (a < b) {
+        std::lock_guard<std::mutex> lock(n);
+        if (*a > t && *a % 2 == 1) t = *a;
+        std::cout << *a << std::endl;
+        a++;
+    }
+}
+                            //указатель на функцию
+void for_each( It a, It b, void(*find0)(It,It)) {
+  auto s = distance(a, b);
+  std::cout << "s=" << s << std::endl;;
+    int v = s / 2;
+    if(v >3){
+    find0( a +  v, b);        
+    std::async(for_each, a + v, b, find);
+    find0(a, a + v - 1);
+    }
+}
+
+#include <iostream>
+
+int main()
+{
+    std::vector<int> arr = { 12,5,46,7,96,7,16,15 , 5, 1 , 9, 7, 11, 5, 6, 87, 21 };
+    for_each( arr.begin(), arr.end(), find);
+    std::cout <<"t=" << t;
+}
+
